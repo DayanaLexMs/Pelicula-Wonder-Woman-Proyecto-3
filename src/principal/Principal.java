2de142e919ad5bdd716d3167c95035ffb337d7fb
Dayana <dayanalexarams@ufps.edu.co>
2026-05-11 12:37:58 -0500

    probando

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package wonderwoman;
+package principal;
 
 /**
  *
  * @author USUARIO
  */
-public class WonderWoman {
+public class Principal {
 
     /**
      * @param args the command line arguments
